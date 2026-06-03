210c0a4a85e4669f15357cc17c937b108b708fb5
carlosgonsalesrios-afk <carlosgonsalesrios@gmail.com>
2026-06-03 06:32:30 -0500

    Update index.js
    Si

--- index.js
+++ index.js
@@ -223,4 +223,18 @@ async function startBot() {
     });
 }
 
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot activo');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor activo en puerto ${PORT}`);
+});
+
 startBot();
